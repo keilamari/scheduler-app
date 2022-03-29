a2e59cbb1dab9736f540eb93321b48dc9dd33d37
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-29 18:13:50 -0400

    added confirm & danger classes to button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,17 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <button>{props.children}</button>;
+   let buttonClass = "button";
+   
+   //when props.confirm is true the button--confirm class is added to the buttonClass
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+
+   //when props.danger is true the button--danger class is added to the buttonClass
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+ 
+   return <button className={buttonClass}>{props.children}</button>;
  }
