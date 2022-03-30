cc233ae2c696dd72563457b12b2be0568001db97
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-30 16:55:17 -0400

    added reset and cancel functions to Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,32 +5,39 @@ import InterviewerList from "components/InterviewerList";
 export default function Form(props){
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-
+  const reset = function() {
+    setStudent("");
+    setInterviewer(null);
+  }
+  const cancel = function() {
+    reset();
+    props.onCancel();
+  }
   return (
-<main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off" onSubmit={evt => evt.preventDefault()}>
-      <input
-        className="appointment__create-input text--semi-bold"
-        name={props.name}
-        type="text"
-        value={student}
-        placeholder="Enter Student Name"
-        onChange={(evt) => setStudent(evt.target.value)}
-      />
-    </form>
-    <InterviewerList 
-      interviewers={props.interviewers} 
-      value={interviewer} 
-      onChange={setInterviewer} 
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={props.onCancel}>Cancel</Button>
-      <Button confirm onClick={props.onSave}>Save</Button>
-    </section>
-  </section>
-</main>
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={evt => evt.preventDefault()}>
+          <input
+            className="appointment__create-input text--semi-bold"
+            name={props.name}
+            type="text"
+            value={student}
+            placeholder="Enter Student Name"
+            onChange={(evt) => setStudent(evt.target.value)}
+          />
+        </form>
+        <InterviewerList 
+          interviewers={props.interviewers} 
+          value={interviewer} 
+          onChange={setInterviewer} 
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
+        </section>
+      </section>
+    </main>
   );
 }
