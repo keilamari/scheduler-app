6a48b9d169ea3a5e93000929d951c0b57533fec6
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-30 15:03:27 -0400

    added formatAppts to Appointment component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./styles.scss";
 
 export default function Appointment(props) {
+  const formatAppts = function(props) {
+    if (!props.time) {
+      return "No Appointments"
+    } else {
+      return `Appointment at ${props.time}`
+    }
+  }
+  
   return (
-    <article className="appointment"></article>
+    <article className="appointment">{formatAppts(props)}</article>
   );
-}
+} 
