dc830244ada856e746d1540e2c7668a275596db6
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-30 09:19:35 -0400

    add props, event handler, classNames to InterviewerListItem component

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,14 +1,20 @@
 import React from "react";
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewListItem(props) {
+  const interviewClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+
   return (
-    <li className="interviewers__item">
+    <li onClick={() => props.setInterviewer(props.id)} className={interviewClass} selected={props.selected}>
   <img
     className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
+    src={props.avatar}
+    alt={props.name}
   />
-  Sylvia Palmer
+  {props.name}
 </li>
   );
 }
