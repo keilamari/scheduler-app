c46664ef34b4af1c758363ce722106fea861d716
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-31 19:58:52 -0400

    removed console.log

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,6 +4,7 @@ import InterviewerListItem from "components/InterviewerListItem";
 
 
 export default function InterviewerList(props) {
+  console.log(props)
   const InterviewersList = props.interviewers.map((interviewer) => (
     <InterviewerListItem 
       key={interviewer.id}
@@ -13,6 +14,7 @@ export default function InterviewerList(props) {
       setInterviewer={() => props.onChange(interviewer.id)}
     />
   ))
+  
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
