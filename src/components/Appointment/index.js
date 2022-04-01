343021718db53aa9ac3408ffbacc07fc49e50965
Keila Kopvillem <kmkopvillem@gmail.com>
2022-03-31 22:44:05 -0400

    added save function and SAVING mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Status from "./Status";
 import useVisualMode from "hooks/useVisualMode";
 
 
@@ -12,9 +13,20 @@ export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
+  const SAVING = "SAVING";
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
+  // console.log(props, "props printed")
+  const save = function(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    transition(SAVING);
+    props.bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+  }
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -30,8 +42,10 @@ export default function Appointment(props) {
         name={props.name}
         value={props.value}
         interviewers={props.interviewers}
-        onCancel={() => back()}
+        onCancel={back}
+        onSave={save}
         />}
+      {mode === SAVING && <Status message="Saving" />}
     </article>
   );
 } 
